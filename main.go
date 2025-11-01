037e5bd4766d335933c91815dfc086ba5fd3db94
yang-yeongjun <dasd412@naver.com>
2025-11-01 11:46:12 +0900

    mutex test

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
-
 func main() {
-    fmt.Println("Hello, World!")
+	var mu SpinMutex
+	counter := 0
+
+	//버퍼 없는 채널이라 블로킹됨
+	done := make(chan bool)
+
+	for i := 0; i < 100; i++ {
+		go func() {
+			for j := 0; j < 1000; j++ {
+				mu.Lock()
+				counter++
+				mu.Unlock()
+			}
+			done <- true //송신 (send): 채널에 값을 넣기 (신호 보내기)
+		}()
+	}
+
+	// 모든 고루틴 완료 대기
+	for i := 0; i < 100; i++ {
+		//수신 (receive): 채널에서 값을 꺼내기
+		<-done // 값을 버림 (동기화 목적, 신호 올 때까지 블로킹)
+	}
+
+	println("Counter:", counter)
 }
--- mutex_test.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "testing"
-
-func TestSomething(t *testing.T) {
-    // 테스트 코드
-}
